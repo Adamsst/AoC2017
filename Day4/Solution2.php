f3d62b987612a787d41383cc831f8404948f6ea3
Adamsst <Adamsst55@gmail.com>
2017-12-05 09:12:33 -0500

    Fixed part 2
    I tried to get fancy with part 2 and while it gave me the correct answer, it wouldn't work for all input. I fixed that.

--- Day4/Solution2.php
+++ Day4/Solution2.php
@@ -1,11 +1,8 @@
 <?php 
 
 //Read each line of input into an array of strings and trim trailing whitespace
-//Set up an associative array using each letter of the alphabet
-//Calculate each letter's value using binary to ensure unique values for all letter combinations
-//sum the character value for every string in each array and create a new array of arrays formed of these values
-//using the array_unique result, compare the result to this new array of numerical values
-//if they are they same, do nothing, else increase the number of invalid sums
+//For each string in each line of input, sort the string alphabetically
+//Use part answer (finding array with unique values) on the newly sorted input
 
 $input = array();
 $lines = file('input.txt');
@@ -15,28 +12,14 @@ foreach($lines as $singleLine){
 	array_push($input,array_map('trim',explode(" ",$singleLine)));
 }
 
-$params = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'); 
-$value = 1;
-$values = array();
-$intValues = array();
-
-for($i=0;$i<count($params);$i++){
-	$values[$params[$i]] = $value;
-	$value = $value * 2;
-} 
-
 for($i=0;$i<count($input);$i++){
 	for($j=0;$j<count($input[$i]);$j++){
-		$total = 0;
-			for($k=0;$k<strlen($input[$i][$j]);$k++){
-				$total += $values[$input[$i][$j][$k]];
-			}
-		$intValues[$i][$j] = $total;
+		$input[$i][$j] = stringSort($input[$i][$j]);
 	}
 }
 
-for($i=0;$i<count($intValues);$i++){
-	$sum += findUnique($intValues[$i]);
+for($i=0;$i<count($input);$i++){
+	$sum += findUnique($input[$i]);
 }
 
 echo (count($input) - $sum);
@@ -50,4 +33,12 @@ function findUnique($a){
 		return 1;
 	}
 } 
+
+function stringSort($s){
+	$string = $s;
+	$stringSplit = str_split($string);
+	sort($stringSplit);
+	return implode('',$stringSplit);
+}
+
 ?>
